b982cb86915f2f76a717959a40e905cfd578cb6d
Naoya Teraoka <naoya.kansai0403@gmail.com>
2021-04-29 18:29:47 +0900

    20210429

--- src/Todo_App/app/contollers/route_main.go
+++ src/Todo_App/app/contollers/route_main.go
@@ -1,6 +1,8 @@
 package contollers
 
 import (
+	"Golang6/src/Todo_App/app/models"
+	"log"
 	"net/http"
 )
 
@@ -14,13 +16,110 @@ func top(w http.ResponseWriter, r *http.Request){
 }
 
 func index(w http.ResponseWriter, r *http.Request){
-	_, err := session(w, r)
+	sess, err := session(w, r)
 	if err != nil {
 		http.Redirect(w, r, "/", 302)
 	} else {
+		user, err := sess.GetUserBySession()
+		if err != nil{
+			log.Fatalln(err)
+		}
+		todos, _ := user.GetTodoByUser()
+		user.Todos = todos
 		generateHTML(w, nil, "layout", "private_navbar", "index")
 	}
 
 }
 
+func todoNew(w http.ResponseWriter, r *http.Request){
+	_, err := session(w, r)
+	if err != nil{
+		http.Redirect(w, r, "login", 302)
+	}else{
+		generateHTML(w, nil, "layout", "todo_new")
+	}
+}
+
+func todoSave(w http.ResponseWriter, r *http.Request){
+	sess, err := session(w,r)
+	if err != nil{
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err = r.ParseForm()
+		if err != nil{
+			log.Println(err)
+		}
+		user, err := sess.GetUserBySession()
+		if err != nil{
+			log.Fatalln(err)
+		}
+		content := r.PostFormValue("content")
+		if err := user.CreateTodo(content); err != nil{
+			log.Fatalln(err)
+		}
+		http.Redirect(w, r, "/todos", 302)
+
+	}
+}
+
+func todoEdit(w http.ResponseWriter, r *http.Request, id int){
+	sess, err := session(w, r)
+	if err != nil{
+		http.Redirect(w, r, "/login", 302)
+	}else {
+		_, err := sess.GetUserBySession()
+		if err != nil{
+			log.Fatalln(err)
+		}
+		t, err := models.GetTodo(id)
+		if err != nil{
+			log.Fatalln(err)
+		}
+		generateHTML(w, t, "layout", "private_nabvar", "todo_edit")
+	}
+}
+
+func todoUpdate(w http.ResponseWriter, r *http.Request, id int){
+	sess, err := session(w, r)
+	if err != nil{
+		http.Redirect(w, r, "/login", 302)
+	}else {
+		err := r.ParseForm()
+		if err != nil{
+			log.Fatalln(err)
+		}
+		user, err := sess.GetUserBySession()
+		if err != nil{
+			log.Fatalln(err)
+		}
+		content := r.PostFormValue("content")
+		t := &models.Todo{ID :id, Content: content, UserID: user.ID}
+		if err := t.UpdateTodo(); err != nil{
+			log.Fatalln(err)
+		}
+		http.Redirect(w, r, "/todos", 302)
+
+	}
+
+}
+
+func todoDelete(w http.ResponseWriter, r *http.Request, id int){
+	sess, err := session(w, r)
+	if err != nil{
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		_, err := sess.GetUserBySession()
+		if err != nil{
+			log.Fatalln(err)
+		}
+		t, err := models.GetTodo(id)
+		if err != nil{
+			log.Fatalln(err)
+		}
+		if err := t.DeleteTodo(); err != nil{
+			log.Fatalln(err)
+		}
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
 
--- src/Todo_App/app/models/users.go
+++ src/Todo_App/app/models/users.go
@@ -12,6 +12,7 @@ type User struct {
 	Email string
 	Password string
 	CreatedAt time.Time
+	Todos []Todo
 }
 
 type Session struct {
@@ -136,7 +137,27 @@ func (sess *Session) CheckSession()(valid bool, err error){
 	return valid, err
 }
 
+func (sess *Session) DeleteSessionByUUID() (err error){
+	cmd := `delete from sessions where uuid = ?`
+	_, err = Db.Exec(cmd, sess.UUID)
+	if err != nil{
+		log.Fatalln(err)
+	}
+	return err
+}
 
+func (sess *Session) GetUserBySession() (user User, err error){
+	user = User{}
+	cmd := `select id, uuid, name, email, created_at, from users
+		   where id = ?`
+	err= Db.QueryRow(cmd, sess.ID).Scan(&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.CreatedAt)
+
+	return user, err
+}
 
 
 
